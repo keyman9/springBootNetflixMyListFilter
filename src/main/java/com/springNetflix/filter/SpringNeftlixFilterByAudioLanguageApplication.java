8c915215ada841dad9c2090343ee292d1a625526
keyman9 <laxscott@gmail.com>
2018-04-19 16:50:50 -0600

    Initial Commit.

---
+++ src/main/java/com/springNetflix/filter/SpringNeftlixFilterByAudioLanguageApplication.java
@@ -0,0 +1,12 @@
+package com.springNetflix.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNeftlixFilterByAudioLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNeftlixFilterByAudioLanguageApplication.class, args);
+	}
+}
